ca4db11964d9d36a29320e9c4baf95c3f32e7ace
Homero Silva <hsilvap@me.com>
2024-08-01 13:17:52 -0600

    try to fix deploy

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { CssBaseline } from "@mui/material";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { RouterProvider } from "react-router-dom";
 import { Amplify } from "aws-amplify";
-import router from "./components/Router";
+import router from "./components/Routes";
 import awsconfig from "./aws";
 
 const queryClient = new QueryClient();
